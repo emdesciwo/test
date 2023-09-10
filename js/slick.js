c768d918cb668b3b5e4691461f7dc546f6d036ad
emdesciwo <143944765+emdesciwo@users.noreply.github.com>
2023-09-10 19:29:39 +0200

    Add files via upload

---
+++ js/slick.js
@@ -0,0 +1,21 @@
+// do pliku slick.js
+
+$(document).ready(function () {
+	$(".reviews__boxes").slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		arrows: false,
+		mobileFirst: true,
+		responsive: [
+			{
+				breakpoint: 700,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				},
+			},
+		],
+	});
+});
